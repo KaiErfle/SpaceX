f3db17a1924b28753ddc674bb06af72fcb3b2ca4
KaiErfle <kaierfle@gmail.com>
2020-04-15 14:25:17 -0700

    button

---
+++ comps/Button/index.js
@@ -0,0 +1,10 @@
+import React from './node_modules/react';
+import './Button.css';
+
+const Button = () => <div>
+    <div>
+        Button 
+    </div>
+</div>
+
+export default Button;
---
+++ stories/Button.stories.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import button from '../comps/Button'
+
+export default {
+    title:"Button",
+    component:Button 
+};
+
+export const MyButton = () => <Button />;
+
