cc123afcf3bd3ba9271f1cfbde1bafd6a9a3cd1a
KaiErfle <kaierfle@gmail.com>
2020-04-15 15:29:32 -0700

    fixed button

---
+++ comps/Button/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Button.css';
+
+const Button = () => <div className="button_box">
+    <div className="button">
+        Button
+    </div>
+</div>
+
+export default Button;
+
+export const num = 0;
+export const greeting = 'hi';
---
+++ stories/2-MyButton.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from '../comps/Button';
+
+export default {
+    title:"My Button",
+    component:Button
+};
+
+export const MyButton = () => <Button />
