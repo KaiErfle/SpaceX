368d596a4507a6986420f5e244b97bcf9fd90f1c
KaiErfle <kaierfle@gmail.com>
2020-05-09 14:02:37 -0700

    heroku bug fixes

--- comps/Test/Physical/index.js
+++ comps/Test/Physical/index.js
@@ -3,51 +3,54 @@ import './Physical.css';
 import Button from '../../Button';
 
 const Physical = () => <div>
+    <div className="physical-test">
 
-<h1>Part 1: Physical</h1>
+        <h1>Part 1: Physical</h1>
 
-<p id="info"> Okay,<br /> 
-first things first, I need to know a bit about your body type!<br />
-Remember to be honest about this portion of the test!
-</p>
-    
-<form id="excercise">
-<p>Do you think you get enough excercise</p>
-<input id="text" type="radio" value="A" />
-<label>A. Yes<br/></label>
+        <p id="info"> Okay,<br /> 
+        first things first, I need to know a bit about your body type!<br />
+        Remember to be honest about this portion of the test!
+        </p>
+            
+        <form id="excercise">
+        <p>Do you think you get enough excercise</p>
+        <input id="text" type="radio" value="A" />
+        <label>A. Yes<br /></label>
 
-<input type="radio" value="B" />
-<label>B. No<br/></label>
+        <input type="radio" value="B" />
+        <label>B. No<br/></label>
 
-</form>
+        </form>
 
-<form id="height">
+        <form id="height">
 
-<p>How tall are you?</p>
-<input type="radio" value="A" />
-<label>A. Short<br/></label>
+        <p>How tall are you?</p>
+        <input type="radio" value="A" />
+        <label>A. Short<br/></label>
 
-<input type="radio" value="B" />
-<label>B. Average<br/></label>
+        <input type="radio" value="B" />
+        <label>B. Average<br/></label>
 
-<input type="radio" value="C" />
-<label>C. Tall<br/></label>
+        <input type="radio" value="C" />
+        <label>C. Tall<br/></label>
 
-</form>
+        </form>
 
-<form id="weight">
+        <form id="weight">
 
-<p>how much do you roughly weigh?</p>
-<input type="radio" value="A" />
-<label>A. 90lbs-120lbs<br/></label>
+        <p>how much do you roughly weigh?</p>
+        <input type="radio" value="A" />
+        <label>A. 90lbs-120lbs<br/></label>
 
-<input type="radio" value="B" />
-<label>B. 130lbs-160lbs<br/></label>
+        <input type="radio" value="B" />
+        <label>B. 130lbs-160lbs<br/></label>
 
-<input type="radio" value="C" />
-<label>C. 170lbs-200lbs<br/></label>
+        <input type="radio" value="C" />
+        <label>C. 170lbs-200lbs<br/></label>
 
-</form>
+        </form>
+
+</div>
 
 
 </div>
