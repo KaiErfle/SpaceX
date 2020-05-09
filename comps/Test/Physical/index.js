e84da4ab360337ec5a9d4873ae5e4ac47331bd7a
KaiErfle <kaierfle@gmail.com>
2020-05-09 14:38:38 -0700

    fixed testpage1 css bug

--- comps/Test/Physical/index.js
+++ comps/Test/Physical/index.js
@@ -4,6 +4,7 @@ import Button from '../../Button';
 
 const Physical = () => <div>
     <div className="physical-test">
+    <div id="form">
 
         <h1>Part 1: Physical</h1>
 
@@ -12,47 +13,47 @@ const Physical = () => <div>
         Remember to be honest about this portion of the test!
         </p>
             
-        <form id="excercise">
-        <p>Do you think you get enough excercise</p>
-        <input id="text" type="radio" value="A" />
-        <label>A. Yes<br /></label>
+        
 
-        <input type="radio" value="B" />
-        <label>B. No<br/></label>
+            <form id="excercise">
+            <p>Do you think you get enough excercise</p>
+            <input type="radio" value="A" />
+            <label>A. Yes<br /></label>
 
-        </form>
+            <input type="radio" value="B" />
+            <label>B. No<br/></label>
 
-        <form id="height">
+            </form>
 
-        <p>How tall are you?</p>
-        <input type="radio" value="A" />
-        <label>A. Short<br/></label>
+            <form id="height">
 
-        <input type="radio" value="B" />
-        <label>B. Average<br/></label>
+            <p>How tall are you?</p>
+            <input type="radio" value="A" />
+            <label>A. Short<br/></label>
 
-        <input type="radio" value="C" />
-        <label>C. Tall<br/></label>
+            <input type="radio" value="B" />
+            <label>B. Average<br/></label>
 
-        </form>
+            <input type="radio" value="C" />
+            <label>C. Tall<br/></label>
 
-        <form id="weight">
+            </form>
 
-        <p>how much do you roughly weigh?</p>
-        <input type="radio" value="A" />
-        <label>A. 90lbs-120lbs<br/></label>
+            <form id="weight">
 
-        <input type="radio" value="B" />
-        <label>B. 130lbs-160lbs<br/></label>
+            <p>how much do you roughly weigh?</p>
+            <input type="radio" value="A" />
+            <label>A. 90lbs-120lbs<br/></label>
 
-        <input type="radio" value="C" />
-        <label>C. 170lbs-200lbs<br/></label>
+            <input type="radio" value="B" />
+            <label>B. 130lbs-160lbs<br/></label>
 
-        </form>
+            <input type="radio" value="C" />
+            <label>C. 170lbs-200lbs<br/></label>
 
-</div>
-
-
+            </form>
+        </div>
+    </div>
 </div>
 
 
